ff28e1807b52a691f63afda746043ec0fd561b20
samuelfrnk <samuelandreas.frank@uzh.ch>
2024-03-12 17:40:01 +0100

    debug setup

--- Scraping.py
+++ Scraping.py
@@ -6,6 +6,8 @@ from bs4 import BeautifulSoup
 def search_duckduckgo(query):
     url = f"https://duckduckgo.com/html/?q=site:pypi.org+{query}+explanation"
     response = requests.get(url)
+    print(response.reason)
+    print(response.text)
     soup = BeautifulSoup(response.text, 'html.parser')
     results = soup.find_all('a', class_='result__url')
     pypi_urls = [link['href'] for link in results if 'pypi.org/project/' in link['href']]
@@ -29,13 +31,12 @@ def main():
     search_word = input("Enter search word: ")
     pypi_urls = search_duckduckgo(search_word)
     package_descriptions = get_package_descriptions(pypi_urls)
+    print(pypi_urls)
+    saved_packages = [pkg for pkg in package_descriptions if True]
 
-    saved_packages = [pkg for pkg in package_descriptions if "certain keyword" in pkg[1]]
-
-
-    with open("saved_packages.txt", "w") as f:
-        for pkg in saved_packages:
-            f.write(f"{pkg[0]}\n{pkg[1]}\n\n")
+    #with open("saved_packages.txt", "w") as f:
+    #    for pkg in saved_packages:
+    #        f.write(f"{pkg[0]}\n{pkg[1]}\n\n")
 
 
 if __name__ == "__main__":
